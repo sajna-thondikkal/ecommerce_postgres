836f1fa569b9b7b24a92bcea06b46b626761b5d8
Sajna <sajnathondikkal@gmail.com>
2024-06-15 18:37:20 -0700

    db config file done

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -5,3 +5,20 @@ const dbConfig = {
     port: 5432,
     database: 'e_commercedb_postgres'
 }
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(dbConfig.database,dbConfig.user,dbConfig.password,{
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+sequelize.authenticate().then((result) => {
+    console.log("Connected successfully");
+    sequelize.sync().then((result) => {
+        console.log("--------Model syncd successfully-----------");
+    }).catch((err) => {
+        console.log("-------------Model not synced----------");
+    });
+}).catch((err) => {
+    console.log("Connection faild");
+});
+module.exports = sequelize;
